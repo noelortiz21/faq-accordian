1d7eac9c41135545d4034abd7e75fb0387f427d4
Noel Ortiz <noel@Noels-MacBook-Pro-2.local>
2024-01-29 19:12:08 -0800

    changed inputs to be type checkbox instead of radio. users can now expand and minimize the  answers

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-const radiobtns = document.querySelectorAll('input[type="radio"]'); 
-let expandIcon=document.querySelectorAll('.expand-icon');
-function handleRadioChange(index) { 
-    expandIcon.forEach((icon, i) => {
-        if (i==index){
-            icon.src= 'assets/images/icon-minus.svg' 
-        }else{
-            icon.src= 'assets/images/icon-plus.svg' 
-        }
-    }); 
-    // expandIcon.src='assets/images/icon-minus.svg' 
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let expandIcon = document.querySelectorAll(".expand-icon");
+let answers = document.querySelectorAll(".ans");
+function handleCheckboxChange(index) {
+  const checkbox = checkboxes[index];
+  const icon = expandIcon[index];
+  const answer = answers[index];
+  console.log(checkbox.checked);
+
+  if (checkbox.checked) {
+    icon.src = "assets/images/icon-minus.svg";
+    answer.classList.add("reveal");
+  } else {
+    icon.src = "assets/images/icon-plus.svg";
+    answer.classList.remove("reveal");
+  }
+  checkboxes.forEach((cbox, i) => {
+    if (i !== index) {
+      console.log(`Checkbox ${i}: checked = ${cbox.checked}`);
+      cbox.checked = false;
+      cbox.checked = false;
+      expandIcon[i].src = "assets/images/icon-plus.svg";
+      answers[i].classList.remove("reveal");
+    }
+  });
 }
-for (let i = 0; i < radiobtns.length; i++) {
-    radiobtns[i].addEventListener('change', (event)=>{
-        handleRadioChange(i);
-    });
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener("click", (event) => {
+    handleCheckboxChange(i);
+  });
 }
